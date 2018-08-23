19b1412f49455dd5489a9b63646e8a0d3b59395f
Diogo Felix <diogo2fex@gmail.com>
2018-08-23 05:37:40 -0300

    ricebr additions

--- bot.py
+++ bot.py
@@ -1,34 +1,46 @@
-from sys import argv as arguments
 import time
-import telepot
+from sys import argv as arguments
+from telepot import glance, Bot as Telepot
 from rivescript import RiveScript
+from dotenv import load_dotenv
+from os import getenv
 
 
 def handle(msg):
     """
     TODO
     """
-    content_type, _chat_type, chat_id = telepot.glance(msg)
+    content_type, _chat_type, chat_id = glance(msg)
 
-    if content_type == 'text':
-        check_msg(chat_id, msg['text'], msg['message_id'])
+    new_member = (True if content_type == 'new_chat_member' else False)
+    msg_text = ('new member' if 'text' not in msg else msg['text'])
+
+    if content_type in ['text', 'new_chat_member']:
+        check_msg(chat_id, msg_text, msg['message_id'], new_member)
 
 
-def check_msg(chat_id, msg, user_id):
+def check_msg(chat_id, msg, user_id, new_member=False):
     """
     TODO
     """
     message = msg.lower().split()
-    res = BOT.reply("localuser", msg)
 
-    if(res != "[ERR: No Reply Matched]"):
-        TELEGRAM.sendMessage(
-            chat_id, res, parse_mode='Markdown', reply_to_message_id=user_id)
+    if new_member or (WHEN_MENTIONED and BOTNAME in message):
+        res = BOT.reply("localuser", msg)
+        if(res != "[ERR: No Reply Matched]"):
+            TELEGRAM.sendMessage(chat_id,
+                                 res,
+                                 parse_mode='Markdown',
+                                 reply_to_message_id=user_id)
 
 
+load_dotenv(verbose=True)
 TOKEN = arguments[1]
+TELEGRAM = Telepot(TOKEN)
 
-TELEGRAM = telepot.Bot(TOKEN)
+BOTNAME = getenv("BOTNAME")
+WHEN_MENTIONED = getenv("MENTION")
+
 TELEGRAM.message_loop(handle)
 print('Listening ...')
 
